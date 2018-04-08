669c0d02fdabafd6024f3a68403691644b85e4cf
Dalton <daltonvaren@gmail.com>
2018-04-08 14:18:43 -0700

    added commits to JS for readability

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+// testing console is working
 console.log("Up and running!");
+//defined cards
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+//loging card that was flipped
 console.log("User flipped " + cardOne);
-console.log("testing");
