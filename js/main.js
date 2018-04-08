975250afc532be6c3b243cac4630c7ba4afc10c1
Dalton <daltonvaren@gmail.com>
2018-04-08 15:06:14 -0700

    created base game logic for cards in play

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 // testing console is working
 console.log("Up and running!");
-//defined cards
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-//loging card that was flipped
-console.log("User flipped " + cardOne);
+console.log("-----------------");
+// an array to hold card values
+var cards = ["queen","queen","king","king"];
+// an array to hold selected cards
+var cardsInPlay = [];
+// var to hold first selected card
+var cardOne = cards[0];
+// pushes the selected card to in play array
+cardsInPlay.push(cardOne);
+//log first card in play
+console.log("User flipped "+ cardsInPlay[0]);
+
+// var to hold second selected card
+var cardTwo = cards[1];
+// push second selected card into in play
+cardsInPlay.push(cardTwo);
+//logs second card in play
+console.log("User flipped "+ cardsInPlay[1]);
+//
+if (cardsInPlay.length === 2){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.")
+    }
+}
