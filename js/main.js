d277b22777ac1a4d2d5855801debd399fc4d664e
Dalton <daltonvaren@gmail.com>
2018-04-08 18:01:58 -0700

    updated array into object for more functionality

--- js/main.js
+++ js/main.js
@@ -2,7 +2,28 @@
 console.log("Up and running!");
 console.log("-----------------");
 // an array to hold card values
-var cards = ["queen","queen","king","king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage:"images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage:"images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage:"images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage:"images/king-of-diamonds.png"
+    }
+];
 // an array to hold selected cards
 var cardsInPlay = [];
 //function to check for match
@@ -18,8 +39,10 @@ var checkForMatch = function(){
 }
 //defined a function for fliping cards
 var flipCard = function (cardId){
-    cardsInPlay.push(cards[cardId]);
-    console.log("User flipped "+cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log("User flipped "+cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
     checkForMatch();
 }
 flipCard(0);
